7a3b293e92bd5a50f8d668b473d869a7a12086e0
Jaman-M <jaman.manik1@gmail.comm>
2022-08-28 17:22:21 +0600

    defining reducers - todo slice

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,12 @@
+export const initialState = [{
+    id: 1,
+    text: 'Learn React js',
+    completed: true
+},
+{
+    id: 2,
+    text: 'learn redux',
+    completed: 'false',
+    color: 'red'
+}
+]
